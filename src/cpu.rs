bb6d4c3190671a2ef6489bb68801852eda85bee0
ctames <ctames@email.wm.edu>
2016-08-18 15:16:18 -0400

    non-memory related instructions implemented, and simple input loop for instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,46 +1,21 @@
+// TODO DOCUMENTATION
+
 use std::fmt;
 
 pub struct Registers {
-	registers: Vec<u16>,
-	pcreg: u16,
-	ireg: u16,
+	pub registers: Vec<i16>,
+	pub pcreg: i16,
+	pub ireg: Instruction,
 }
 
-// TODO do I really need getters and setters? probably unnecessary code, information hiding not really important here
 impl Registers {
 	// register[0] is a 0 register
 	pub fn new() -> Registers {
 		Registers { 
 			registers: vec![0; 8],
 			pcreg: 0,
-			ireg: 0,
+			ireg: Instruction::new(0b0000000000000000),
 		}
-	}
-
-	pub fn getreg(self, regnum: usize) -> u16 {
-		// ! don't allow values outside of 0:8
-		self.registers[regnum]
-	}
-
-	pub fn setreg(&mut self, regnum: usize, val: u16) { 
-		// ! don't allow values outside of 1:8	
-		self.registers[regnum] = val; 
-	}
-	
-	pub fn get_pcreg(self) -> u16 {
-		self.pcreg
-	}
-
-	pub fn set_pcreg(&mut self, val: u16) {
-		self.pcreg = val;
-	}
-	
-	pub fn get_ireg(self) -> u16 {
-		self.ireg
-	}
-
-	pub fn set_ireg(&mut self, val: u16) {
-		self.ireg = val;
 	}
 }
 
@@ -56,10 +31,11 @@ impl fmt::Display for Registers {
 }
 
 #[derive(Debug)]
+#[derive(PartialEq)]
 enum Format {
 	RRR,
 	RRI,
-	RI,
+	RI
 }
 
 #[derive(Debug)]
@@ -71,35 +47,36 @@ enum Operation {
 	SW,
 	LW,
 	BEQ,
-	JALR,
+	JALR
 }
 
 pub struct Instruction {
+	binary_rep: u16,
 	format: Format,
 	op: Operation,
-	regA: u8,
-	regB: u8,
-	regC: u8,
-	immed: u16,
+	reg_a: usize,
+	reg_b: usize,
+	reg_c: usize,
 	s_immed: i8,
+	u_immed: u16,
 }
 
 impl Instruction {
-	// TODO finish
 	pub fn new(binary: u16) -> Instruction {
 		let mut new_instruction = Instruction { 
+			binary_rep: binary, 
 			format: Format::RRR,
 			op: Operation::ADD,
-			regA: 0,
-			regB: 0,
-			regC: 0,
-			immed: 0,
-			s_immed: 0
+			reg_a: 0,
+			reg_b: 0,
+			reg_c: 0,
+			s_immed: 0,
+			u_immed: 0
 		};
 		
 		// get opcode to set operation/format
-		match ((binary & 0b1110000000000000) >> 13) {
-			0b000 => continue, 
+		match (binary & 0b1110000000000000) >> 13 {
+			0b000 => {}, 
 			0b001 => {new_instruction.format = Format::RRI; new_instruction.op = Operation::ADDI},
 			0b010 => new_instruction.op = Operation::NAND,
 			0b011 => {new_instruction.format = Format::RI; new_instruction.op = Operation::LUI},
@@ -107,41 +84,79 @@ impl Instruction {
 			0b101 => {new_instruction.format = Format::RRI; new_instruction.op = Operation::LW},
 			0b110 => {new_instruction.format = Format::RRI; new_instruction.op = Operation::BEQ},
 			0b111 => new_instruction.op = Operation::JALR,
+			_ => println!("error: couldn't determine instruction format"),
 		} 
 
 		// get regA value
-		new_instruction.regA = (binary & 0b0001110000000000) >> 10; 
+		new_instruction.reg_a = ((binary & 0b0001110000000000) >> 10) as usize; 
 
 		// if RRR or RRI type, get regB value
 		if new_instruction.format == Format::RRR || new_instruction.format == Format::RRI {
-			new_instruction.regB = (binary & 0b0000001110000000) >> 7;			
+			new_instruction.reg_b = ((binary & 0b0000001110000000) >> 7) as usize;			
 		} 
 		
 		// if RRR
 		if new_instruction.format == Format::RRR {
-
+			new_instruction.reg_c = (binary & 0b0000000000000111) as usize;
 		}
 
 		// if RRI
 		if new_instruction.format == Format::RRI {
-
+			new_instruction.s_immed = (binary & 0b0000000001111111) as i8;
 		}
 		
 		// if RI
 		if new_instruction.format == Format::RI {
-
+			new_instruction.u_immed = (binary & 0b0000001111111111) as u16;
 		}
 		
 		new_instruction
 	}
-
-	// TODO getters and setters? if I decide to remove those for Registers, will leave them out here as well 
-
 }	
 	
-// TODO implement
 impl fmt::Display for Instruction {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "\n") 
+		write!(f, "{:0b}\n format: {:?}\n op: {:?}\n reg_a: {}\n reg_b: {}\n reg_c: {}\n s_immed: {}\n u_immed: {}\n",
+		self.binary_rep, self.format, self.op, self.reg_a, self.reg_b, self.reg_c, self.s_immed, self.u_immed) 
+	}
+}
+
+// TODO finish
+pub fn execute(cpuregs: &mut Registers) {
+	let ref instr = cpuregs.ireg; 
+	let ref mut regs = cpuregs.registers; 
+	match instr.op {
+		Operation::ADD => {
+			if instr.reg_a != 0 {
+				regs[instr.reg_a] = regs[instr.reg_b] + regs[instr.reg_c];	
+			}
+		},
+		Operation::ADDI => {
+			if instr.reg_a != 0 {
+				regs[instr.reg_a] = regs[instr.reg_b] + instr.s_immed as i16;
+			}
+		},
+		Operation::NAND => {
+			if instr.reg_a != 0 {
+				regs[instr.reg_a] =  !(regs[instr.reg_b] & regs[instr.reg_c]);
+			}
+		},
+		Operation::LUI => {
+			if instr.reg_a != 0 {
+				regs[instr.reg_a] = (regs[instr.reg_b] << 6) as i16;
+			}
+		},
+		Operation::SW => {
+			// TODO implement after Main Memory is implemented
+		},
+		Operation::LW => {
+			// TODO implement after Main Memory is implemented
+		},
+		Operation::BEQ => {
+			// TODO implement after Main Memory is implemented
+		},
+		Operation::JALR => {
+			// TODO implement after Main Memory is implemented
+		},
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,40 @@
 mod cpu;
 use std::io;
+use std::u16; 
+use std::string::String;
 
-// TODO input loop allowing binary instructions to be typed in and executed
+/******
+Currently, the program is merely an input loop allowing binary instructions to be typed in and executed
+either an instruction, "quit", or "status" can be input
+instructions are executed, and then the state of the machine is output
+"quit", obviously, exits the program
+"status" outputs machine state 
+******/
 fn main() {
 	let mut cpuregs = cpu::Registers::new(); 
 	println!("{}", cpuregs);	
 	
-	let mut input = String::new();
-	for {
-		try!(io::stdin().read_line(&mut input)); 
+	loop {
+		let mut input = String::new();
+		match io::stdin().read_line(&mut input) {
+			Ok(n) => {
+				match input.trim() {
+					"quit" => break,
+					"status" => println!("{}", cpuregs),
+					_ => {
+						match u16::from_str_radix(&input.trim(), 2) {
+							Ok(binary_instr) => {
+								let instr: cpu::Instruction = cpu::Instruction::new(binary_instr);
+								cpuregs.ireg = instr;
+								cpu::execute(&mut cpuregs); 
+								println!("{}", cpuregs);
+							},
+							Err(error) => println!("error: {}", error), 
+						}
+					},
+				}
+			},
+			Err(error) => println!("error: {}", error),
+		} 
 	}
 }
